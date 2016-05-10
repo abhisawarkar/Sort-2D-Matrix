6cf36a620daf4682096d96c718bcd8886b4d7b66
Abhishek Sawarkar <sawarkar.ab@husky.neu.edu>
2016-05-09 22:22:25 -0700

    Update 2D-sort.py

--- 2D-sort.py
+++ 2D-sort.py
@@ -1,12 +1,12 @@
 #!/bin/python
-import sys
-L=[[9,1,8,5],[2,7,3,11],[6,4,5,14]]
-G=list()
-for i in xrange(len(L)):
-	G.extend(L[i])
-G.sort()
-Final=list()
-for i in xrange(0,len(G),len(L[0])):
-	Final.append(G[i:i+len(L[0])])
-print Final	
+
+def sort_2D(L):
+	G=list()
+	for i in xrange(len(L)):
+		G.extend(L[i])		# Store as 1-D
+	G.sort()			
+	Final=list()
+	for i in xrange(0,len(G),len(L[0])):
+		Final.append(G[i:i+len(L[0])])		# Store back as 2-D
+	return Final	
 		
